dce2a8d6c493186ecfe7db4b98548d12c6a83e42
Cristina-Jordi <you@example.com>
2023-03-08 18:46:44 +0100

    Animated Card Stack

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;  // Variable wird mit false initialisiertund bindet eine weitere css-Klasse ein.
+  currentCard: string | undefined = '';
+  game: Game = new Game();
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+
+      console.log('New card:', this.currentCard);
+      console.log('Game is', this.game);
+
+      setTimeout(() => {
+        if (this.currentCard) {
+          this.game.playedCards.push(this.currentCard);  // Erst wenn die Animation beendet ist, wird die playedCard ins Array gepusht
+        }
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
+  }
 
 }
---
+++ src/models/game.ts
@@ -0,0 +1,35 @@
+export class Game {
+    public players: string[] = [];  // public, weil wir in anderen Dateien auch darauf zugreifen wollen
+    public stack: string[] = [];   // ungespielte Karten
+    public playedCards: string[] = [];
+    public currentPlayer: number = 0;
+
+    //Variante Junus:
+    // constructor() {  // Hier kommen Funktionen hin, welche am Anfang immer aufgerufen werden wo gewisse Logiken aufgerufen werden können
+    //     for(let i=1; i < 14; i++){
+    //         this.stack.push('hearts_' + i);
+    //         this.stack.push('ace_' + i);
+    //         this.stack.push('dimaonds_' + i);
+    //         this.stack.push('clubs_' + i);
+    //     }
+    //  }
+
+    constructor() {
+        const suits = ['hearts', 'ace', 'diamonds', 'clubs'];
+        for (let i = 1; i <= 13; i++) {
+            suits.forEach(suit => {
+                this.stack.push(`${suit}_${i}`);
+            });
+        }
+        this.shuffleCards();
+    }
+
+
+    private shuffleCards() {
+        for (let i = this.stack.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.stack[i], this.stack[j]] = [this.stack[j], this.stack[i]];
+        }
+
+    }
+}
